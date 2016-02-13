dc37771eb6d6f540fbe825c24300f77efd206253
kgrodd <kgrodd@purdue.edu>
2016-02-13 18:50:01 -0500

    heapfile.java template

--- CS448_Spring2016_Project2/HeapFile/src/heap/HeapFile.java
+++ CS448_Spring2016_Project2/HeapFile/src/heap/HeapFile.java
@@ -1,6 +1,40 @@
-public HeapFile(String name)
-public RID insertRecord(byte[] record)public Tuple getRecord(RID rid)
-public void updateRecord(RID rid, Tuple newRecord)
-public void deleteRecord(RID rid)
-public int getRecCnt() //get number of records in the file
-public HeapScan openScan()
+package heapfile;
+
+import global.Convert;
+import global.GlobalConst;
+import global.Minibase;
+import global.Page;
+import global.PageId;
+import heap.HeapFile;
+import heap.HeapScan;
+import heap.Tuple;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
+import chainexception.ChainException;
+public class HeapFile{
+
+	/* If the given name already denotes a file, open it;
+	otherwise, create new empty file*/
+	public HeapFile(String name){}
+	
+	/*Inserts a new record into the flie and returns its RID*/
+	public RID insertRecord(byte[] record){}
+	
+	/* */
+	public Tuple getRecord(RID rid){}
+	
+	/*Updates the specified record in the heap file */
+	public void updateRecord(RID rid, Tuple newRecord){}
+	
+	/* Deletes the specified record from the Heap file*/
+	public void deleteRecord(RID rid){}
+	
+	/* gets the number of records in the file */
+	public int getRecCnt(){}
+	
+	/* Initiates a sequential scan of the heap file */
+	public HeapScan openScan(){}
+}
