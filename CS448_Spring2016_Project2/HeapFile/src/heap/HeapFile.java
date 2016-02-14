b1a10bf55f6ca289ca76af22ed7a3bf0d3f201fd
kgrodd <kgrodd@purdue.edu>
2016-02-13 22:00:26 -0500

    fixed my typo. Oooops

--- CS448_Spring2016_Project2/HeapFile/src/heap/HeapFile.java
+++ CS448_Spring2016_Project2/HeapFile/src/heap/HeapFile.java
@@ -1,4 +1,10 @@
 /* HeapFile.java */
+/* A heap File is an unordered set of records, stored on a set of pages.
+ * This class provides basic support for inserting, updating, and deleting
+ * records.
+ * Temporary heap files are used for external sorting and in other 
+ * relational operators.  A sequential scan of a heap file (via the
+ * scan class) is the most basic access method */ 
 package heap;
 
 import global.Convert;
@@ -8,13 +14,19 @@ import global.Page;
 import global.PageId;
 import global.RID;
 
+import diskmgr.DiskMgr;
+
 
 import java.io.File;
 import java.io.IOException;
 import java.io.RandomAccessFile;
 
 import chainexception.ChainException;
+
 public class HeapFile{
+	HeapListNode FreeSpace; 	//Linked list of pages that have free space
+	HeapListNode noFreeSpace; 	//Linked List of pages that do not have free space
+
 
 	/* If the given name already denotes a file, open it;
 	otherwise, create new empty file*/
